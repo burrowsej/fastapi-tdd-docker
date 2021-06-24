ff8c857b10898b10d50cc08728a039715dc52ab5
ed <edward.burrows@ghd.com>
2021-06-24 15:48:19 +1000

    onto deployment!

--- project/app/main.py
+++ project/app/main.py
@@ -1,9 +1,10 @@
 import os
+import logging
 
 from fastapi import FastAPI, Depends
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -12,6 +13,11 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router,
+        prefix="/summaries",
+        tags=["summaries"],
+    )
 
     return application
 
